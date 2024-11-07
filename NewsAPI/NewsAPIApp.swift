d48d676922deb1f63471d428776c3fb72de78632
Chirag Arora <arora2002chirag@gmail.com>
2024-11-07 11:30:39 +0530

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by chirag arora on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
